b931c44f40f6439f5f90bc78d4684ed108dfc4a1
mikamikachu-cell <81223524+mikamikachu-cell@users.noreply.github.com>
2022-06-05 17:44:33 +0200

    Add files HTML5,CSS3, ES6
    Add index.html, style.css, and index.js

---
+++ drawing/js/index.js
@@ -0,0 +1,57 @@
+console.log('drawing');
+
+const canvas = document.getElementById('drawing-board');
+const toolbar = document.getElementById('toolbar');
+
+const ctx = canvas.getContext('2d');
+
+const canvasOffsetX = canvas.offsetLeft;
+const canvasOffsety = canvas.offsetTop;
+
+canvas.width = window.innerWidth - canvasOffsetX;
+canvas.height = window.innerHeight - canvasOffsety;
+
+let isPainting = false;
+let lineWidth = 5;
+let startX;
+let startY;
+
+toolbar.addEventListener('click', e => {
+    if (e.target.id === 'clear') {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+});
+
+toolbar.addEventListener('change', e => {
+    if (e.target.id === 'stroke') {
+        ctx.strokeStyle = e.target.value;
+    }
+    if (e.target.id === 'lineWidth') {
+        lineWidth = e.target.value;
+    }
+});
+
+canvas.addEventListener('mousedown', (e) => {
+    isPainting = true;
+    startX = e.clientX;
+    startY = e.clientY;
+});
+
+canvas.addEventListener('mouseup', e => {
+    isPainting = false;
+    ctx.stroke();
+    ctx.beginPath();
+});
+
+
+const draw = (e) => {
+    if (!isPainting) {
+        return;
+    }
+    ctx.lineWidth = lineWidth;
+    ctx.lineCap = 'round';
+    
+    ctx.lineTo(e.clientX - canvasOffsetX, e.clientY);
+    ctx.stroke();
+}
+canvas.addEventListener('mousemove', draw);
